5b58de3dd170518e804227d0707c43dbb0b502cf
Cooper Anderson <cooper@cooperstuff.ninja>
2017-11-20 12:52:01 -0800

    Made brew installation prettier

--- brew.sh
+++ brew.sh
@@ -1,1 +1,1 @@
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" >> /dev/null

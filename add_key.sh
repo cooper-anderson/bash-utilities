a86676ed3c8c843d2b91faab28d48b5e43e65ff0
Cooper Anderson <cooper@cooperstuff.ninja>
2017-11-20 12:34:09 -0800

    Clause for adding key
    Only add key if it does exist yet

--- add_key.sh
+++ add_key.sh
@@ -1,1 +1,9 @@
-curl -sL https://raw.githubusercontent.com/cooper-anderson/bash-utilities/master/id_rsa.pub >> ~/.ssh/authorized_keys
+#!/bin/bash
+
+KEY=$(curl -sL https://raw.githubusercontent.com/cooper-anderson/bash-utilities/master/id_rsa.pub)
+touch ~/.ssh/authorized_keys
+
+if [ $(cat ~/.ssh/authorized_keys | grep -c "$KEY") -eq 0 ]; then
+	echo $KEY >> ~/.ssh/authorized_keys
+fi
+

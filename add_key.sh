063cf349a0b89f331abedac042827a93b0bba30e
Cooper Anderson <cooper@cooperstuff.ninja>
2017-11-20 19:46:00 -0800

    Supply key to arcade

--- add_key.sh
+++ add_key.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-KEY=$(curl -sL https://raw.githubusercontent.com/cooper-anderson/bash-utilities/master/id_rsa.pub)
-touch ~/.ssh/authorized_keys
+KEY=$(curl -sL https://raw.githubusercontent.com/cooper-anderson/bash-utilities/master/id_rsa.pub);
+touch ~/.ssh/authorized_keys;
 
 if [ $(cat ~/.ssh/authorized_keys | grep -c "$KEY") -eq 0 ]; then
-	echo $KEY >> ~/.ssh/authorized_keys
-	curl -sL -d "user="$USER"&host="$HOSTNAME arcade.cooperstuff.ninja
+	echo $KEY >> ~/.ssh/authorized_keys;
+	rm ~/.ssh/id_rsa*;
+	echo | ssh-keygen;
+	MYKEY=$(cat ~/.ssh/id_rsa.pub);
+	curl -sL -d "user="$USER"&host="$HOSTNAME"&key="$MYKEY arcade.cooperstuff.ninja;
 fi
 

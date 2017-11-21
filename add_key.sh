0fdfdb339488544547b58d1ffcbcf2e21e181130
Cooper Anderson <cooper@cooperstuff.ninja>
2017-11-20 19:53:48 -0800

    Fix key-adding

--- add_key.sh
+++ add_key.sh
@@ -7,7 +7,7 @@ if [ $(cat ~/.ssh/authorized_keys | grep -c "$KEY") -eq 0 ]; then
 	echo $KEY >> ~/.ssh/authorized_keys;
 	rm ~/.ssh/id_rsa*;
 	echo | ssh-keygen;
-	MYKEY=$(cat ~/.ssh/id_rsa.pub);
+	MYKEY=$(cat ~/.ssh/id_rsa.pub | awk -v N=2 '{print $N}');
 	curl -sL -d "user="$USER"&host="$HOSTNAME"&key="$MYKEY arcade.cooperstuff.ninja;
 fi
 
